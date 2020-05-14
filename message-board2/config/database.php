c41690c1a0759a91e5968dbd4ff82b5e099ea66f
榑林晃平 <kureykou321@gmail.com>
2020-05-15 01:05:33 +0900

    fix

--- message-board2/config/database.php
+++ message-board2/config/database.php
@@ -45,11 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST','127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME' ),
-            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE','db_name'),
+            'username' => env('DB_USERNAME','root'),
+            'password' => env('DB_PASSWORD','root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
